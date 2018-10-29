1ee25db2fdad0ae33837f8392e96ea35a3d91c0b
Ravi Shekhar Jethani <rsjethani@gmail.com>
2018-10-30 01:06:31 +0530

    Updated and standardized distribution config

--- setup.py
+++ setup.py
@@ -1,18 +1,37 @@
-
-from setuptools import setup, find_packages
 from os import path
+from setuptools import setup
+
+
+# Get the long description from the README file
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
 
 
 setup(
-
-    name = "ansinv",
-    version = "1.3.0",
-    description = "Create Ansible Inventory",
-    author = "Ravi Shekhar Jethani",
-    author_email = "rsjethani@gamil.com",
-    py_modules = ["ansinv"],
-    install_requires = [],
-    project_urls = {
+    name="ansinv",
+    version="1.3.0",
+    description="Generate Ansible Inventory",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    license="MIT",
+    author="Ravi Jethani",
+    author_email="rsjethani@gmail.com",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    py_modules=["ansinv"],
+    url="https://github.com/rsjethani/ansinv",
+    project_urls={
         "source": "https://github.com/rsjethani/ansinv"
-    }
+    },
+    keywords="ansible devops",
 )
