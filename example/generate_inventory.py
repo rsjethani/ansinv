91527ea98f5a85d8047abd5ff1b70fb2d7723092
Ravi Shekhar Jethani <rsjethani@gmail.com>
2019-01-05 20:54:13 +0530

    Modify example according to new ansinv code (#4)

--- example/generate_inventory.py
+++ example/generate_inventory.py
@@ -3,42 +3,39 @@
 # PS: No exception/error handling present in order to keep things simple
 
 import json
-
-
-import ansinv
+from ansinv import *
 
 
 # Initialize empty inventory
-inventory = ansinv.AnsibleInventory()
+inventory = AnsibleInventory()
 
 # Create required groups
-for group in "manager", "worker", "monitoring", "cluster":
-    inventory.add_group(group)
+for grp_name in "master", "worker", "monitoring", "k8s-cluster":
+    inventory.add_groups(AnsibleGroup(grp_name))
 
 # Establish parent-child relation if any between groups
-inventory.add_children_to_group("cluster", "manager", "worker", "monitoring")
+inventory.group("k8s-cluster").add_children(inventory.group("master"), inventory.group("worker"))
 
 # Add groupvars if any to groups
-inventory.update_groupvars("monitoring", elastic_port=6000, logstash_port=6100, kibana_port=3000)
-inventory.update_groupvars("all", ansible_user="testuser", ansible_ssh_private_key_file="cloud.key")
+inventory.group("monitoring").groupvars.update(elastic_port=6000, logstash_port=6100, kibana_port=3000)
+inventory.group("all").groupvars.update(ansible_user="testuser", ansible_ssh_private_key_file="cloud.key")
 
 # load terraform state file
 with open("terraform.tfstate.example") as file:
     state = json.load(file)
 
-# Add hosts to inventory
+# Add hosts to groups and/or inventory
 for resource, res_info in state["modules"][0]["resources"].items():
     if res_info["type"] == "libvirt_domain":
         host_ip = res_info["primary"]["attributes"]['network_interface.0.addresses.0']
+        name = res_info["primary"]["attributes"]['name']
 
         # "libvirt_domain.worker.0" -> "libvirt_domain", "worker", ("0",)
-        _, group, *num = resource.split(".")
-        num = num[0] if len(num) != 0 else "0"
+        grp_name = resource.split(".")[1]
 
         # add host to repective group
-        inventory.add_host(host_ip, host_name="{}-{}".format(group, num))
-        inventory.add_hosts_to_group(group, host_ip)
+        inventory.group(grp_name).add_hosts(AnsibleHost(host_ip, host_name=name))
 
 # Write inventory data to a file in ini format.
 with open("inventory.example", "w") as file:
-    file.write(inventory.as_ini())
+    file.write(str(inventory))
